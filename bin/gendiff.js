bf60f1b925506389af82669e6bef658e077ac71c
Dmitry Usov <usovdm@gmail.com>
2020-12-07 20:04:26 +0300

    gets version from package.json

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,11 +1,9 @@
 #!/usr/bin/env node
-
-const { Command } = require('commander');
-
-const program = new Command();
+const { program } = require('commander');
+const { version } = require('../package.json');
 
 program
-  .version('0.0.1')
+  .version(version)
   .description('Compares two configuration files and shows a difference');
 
 program.parse(process.argv);
