638e91dc23a32ec9547c97330a5a72122057df55
Dmitry Usov <usovdm@gmail.com>
2020-12-07 19:53:43 +0300

    installs eslint and fixes the problems

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,10 +1,11 @@
 #!/usr/bin/env node
 
 const { Command } = require('commander');
+
 const program = new Command();
 
 program
   .version('0.0.1')
-  .description('Compares two configuration files and shows a difference')
+  .description('Compares two configuration files and shows a difference');
 
 program.parse(process.argv);
