1089675c422a27522c9b9cad28cce5ad9d91192f
Dmitry Usov <usovdm@gmail.com>
2020-12-07 22:05:03 +0300

    adds arguments

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -9,6 +9,7 @@ const { program } = commander;
 
 program
   .version(version)
+  .arguments('[options] <filepath1> <filepath2>')
   .description('Compares two configuration files and shows a difference');
 
 program.parse(process.argv);
