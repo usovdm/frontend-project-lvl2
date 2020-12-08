20b11bdd4f48177aab3cd35297ca3cd5f8cf2301
Dmitry Usov <usovdm@gmail.com>
2020-12-08 12:13:39 +0300

    adds option format

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -10,6 +10,7 @@ const { program } = commander;
 program
   .version(version)
   .arguments('[options] <filepath1> <filepath2>')
-  .description('Compares two configuration files and shows a difference');
+  .description('Compares two configuration files and shows a difference')
+  .option('-f, --format [type]', 'output format');
 
 program.parse(process.argv);
