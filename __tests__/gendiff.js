9da3a784a3ff471f21536151a4dc8c7b2736db1c
Dmitry Usov <usovdm@gmail.com>
2020-12-10 22:07:26 +0300

    notes test cases

--- __tests__/gendiff.js
+++ __tests__/gendiff.js
@@ -1,5 +1,19 @@
-describe('jest', () => {
-  it('works', () => {
-    expect(true).toBeTruthy();
+describe('gendiff', () => {
+  describe('files', () => {
+    it.skip('reads both files', () => { });
+    it.skip('fails if the first file has the wrong path', () => { });
+    it.skip('fails if the second file has the wrong path', () => { });
+  });
+  describe('function', () => {
+    it.skip('returns unchanged property', () => { });
+    it.skip('returns added property', () => { });
+    it.skip('returns removed property', () => { });
+    it.skip('returns complex changes', () => { });
+    it.skip('returns changes where null is changed', () => { });
+    it.skip('returns changes where bool is changed', () => { });
   });
 });
+
+/*
+extra ideas of cases
+*/
